4e0213a3de0565315d12aae0373bf240f61a9e7e
PasinduManeka <manekaherat815@gmail.com>
2021-04-21 21:35:22 +0530

    create database connection.

--- GadgetBadget/src/model/Fund.java
+++ GadgetBadget/src/model/Fund.java
@@ -1,5 +1,19 @@
 package model;
 
-public class Fund {
+import java.sql.Connection;
+import java.sql.DriverManager;
 
+public class Fund {
+	
+	public Connection connect() {
+		Connection con = null;
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/test", "root", "");
+			
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+		return con;
+	}
 }
