3bf8e6548b8c21026e6f2199ed01dc1542930b00
udeepa-des <ududeepa@gmail.com>
2021-04-21 19:12:45 +0530

    code added

--- GadgetBadget/src/com/CartService.java
+++ GadgetBadget/src/com/CartService.java
@@ -1,5 +1,75 @@
 package com;
 
+import model.Cart;
+//For REST Service
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+
+//For JSON
+import com.google.gson.*;
+
+//For XML
+import org.jsoup.*;
+import org.jsoup.parser.*;
+import org.jsoup.nodes.Document;
+
+@Path("/carts")
 public class CartService {
 
+	Cart cartObj = new Cart();
+	@GET
+	@Path("/")
+	@Produces(MediaType.TEXT_HTML)
+	public String readItems()
+	{
+		return cartObj.readItems();
+	}
+	
+	@POST
+	@Path("/")
+	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	@Produces(MediaType.TEXT_PLAIN)
+	public String insertItem(@FormParam("projCode") String projCode,
+			@FormParam("projName") String projName,
+			@FormParam("Amount") String Amount,
+			@FormParam("Description") String Description)
+	{
+		String output = cartObj.insertItem(projCode, projName, Amount, Description);
+		return output;
+	}
+	
+	@PUT
+	@Path("/")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.TEXT_PLAIN)
+	public String updateItem(String cartData)
+	{
+		//Convert the input string to a JSON object
+		JsonObject cartObject = new JsonParser().parse(cartData).getAsJsonObject();
+		//Read the values from the JSON object
+		String ID = cartObject.get("ID").getAsString();
+		String projCode = cartObject.get("projCode").getAsString();
+		String projName = cartObject.get("projName").getAsString();
+		String Amount = cartObject.get("Amount").getAsString();
+		String Description = cartObject.get("Description").getAsString();
+		String output = cartObj.updateItem(ID, projCode, projName, Amount, Description);
+		return output;
+	}
+	
+	@DELETE
+	@Path("/")
+	@Consumes(MediaType.APPLICATION_XML)
+	@Produces(MediaType.TEXT_PLAIN)
+	public String deleteItem(String cartData)
+	{
+		//Convert the input string to an XML document
+		Document doc = Jsoup.parse(cartData, "", Parser.xmlParser());
+		//Read the value from the element <itemID>
+		String ID = doc.select("ID").text();
+		String output = cartObj.deleteItem(ID);
+		return output;
+	}
+	
 }
+
+
--- GadgetBadget/src/model/Cart.java
+++ GadgetBadget/src/model/Cart.java
@@ -1,5 +1,160 @@
 package model;
 
-public class Cart {
+import java.sql.*;
 
+public class Cart {
+	//A common method to connect to the DB
+	private Connection connect()
+	{
+		Connection con = null;
+		try
+		{
+			Class.forName("com.mysql.jdbc.Driver");
+			//Provide the correct details: DBServer/DBName, username, password
+			con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/paf_project", "root", "");
+		}
+		catch (Exception e)
+		{e.printStackTrace();}
+		return con;
+	}
+	public String insertItem(String code, String name, String price, String desc)
+	{
+		String output = "";
+		try
+		{
+			Connection con = connect();
+			if (con == null)
+			{return "Error while connecting to the database for inserting."; }
+			// create a prepared statement
+			String query = " insert into cart"
+					+ "			(`ID`,`projCode`,`projName`,`Amount`,`Description`)"
+					+ " values (?, ?, ?, ?, ?)";
+			PreparedStatement preparedStmt = con.prepareStatement(query);
+			// binding values
+			preparedStmt.setInt(1, 0);
+			preparedStmt.setString(2, code);
+			preparedStmt.setString(3, name);
+			preparedStmt.setDouble(4, Double.parseDouble(price));
+			preparedStmt.setString(5, desc);
+			// execute the statement
+			preparedStmt.execute();
+			con.close();
+			output = "Inserted successfully";
+		}
+		catch (Exception e)
+		{
+			output = "Error while inserting the item.";
+			System.err.println(e.getMessage());
+		}
+		return output;
+	}
+	public String readItems()
+	{
+		String output = "";
+		try
+		{
+			Connection con = connect();
+			if (con == null)
+			{return "Error while connecting to the database for reading."; }
+			// Prepare the html table to be displayed
+			output = "<table border='1'><tr><th>proj Code</th><th>proj Name</th>" +
+					"<th>Amount</th>" +
+					"<th>Description</th>" +
+					"<th>Update</th><th>Remove</th></tr>";
+			String query = "select * from cart";
+			Statement stmt = con.createStatement();
+			ResultSet rs = stmt.executeQuery(query);
+			// iterate through the rows in the result set
+			while (rs.next())
+			{
+				String ID = Integer.toString(rs.getInt("ID"));
+				String projCode = rs.getString("projCode");
+				String projName = rs.getString("projName");
+				String Amount = Double.toString(rs.getDouble("Amount"));
+				String Description = rs.getString("Description");
+				// Add into the html table
+				output += "<tr><td>" + projCode + "</td>";
+				output += "<td>" + projName + "</td>";
+				output += "<td>" + Amount + "</td>";
+				output += "<td>" + Description + "</td>";
+				// buttons
+				output += "<td><input name='btnUpdate' type='button' value='Update' "
+						+ "class='btn btn-secondary'></td>"
+								+ "<td><form method='post' action='items.jsp'>"
+								+ "<input name='btnRemove' type='submit' value='Remove'"
+								+ "class='btn btn-danger'>"
+										+ "<input name='ID' type='hidden' value='" + ID
+										+ "'>" + "</form></td></tr>";
+			}
+			con.close();
+			// Complete the html table
+			output += "</table>";
+		}
+		catch (Exception e)
+		{
+			output = "Error while reading the items.";
+			System.err.println(e.getMessage());
+		}
+		return output;
+	}
+	public String updateItem(String ID, String code, String name, String price, String desc)
+	
+	{
+		String output = "";
+		try
+		{
+			Connection con = connect();
+			if (con == null)
+			{return "Error while connecting to the database for updating."; }
+			// create a prepared statement
+			String query = "UPDATE cart SET projCode=?,projName=?,Amount=?,Description=?"
+					+ "WHERE ID=?";
+							PreparedStatement preparedStmt = con.prepareStatement(query);
+			// binding values
+			preparedStmt.setString(1, code);
+			preparedStmt.setString(2, name);
+			preparedStmt.setDouble(3, Double.parseDouble(price));
+			preparedStmt.setString(4, desc);
+			preparedStmt.setInt(5, Integer.parseInt(ID));
+			// execute the statement
+			preparedStmt.execute();
+			con.close();
+			output = "Updated successfully";
+		}
+		catch (Exception e)
+		{
+			output = "Error while updating the item.";
+			System.err.println(e.getMessage());
+		}
+		return output;
+	}
+	
+	public String deleteItem(String cartID)
+	{
+		String output = "";
+		try
+		{
+			Connection con = connect();
+			if (con == null)
+			{return "Error while connecting to the database for deleting."; }
+			// create a prepared statement
+			String query = "delete from cart where ID=?";
+			PreparedStatement preparedStmt = con.prepareStatement(query);
+			// binding values
+			preparedStmt.setInt(1, Integer.parseInt(cartID));
+			// execute the statement
+			preparedStmt.execute();
+			con.close();
+			output = "Deleted successfully";
+		}
+		catch (Exception e)
+		{
+			output = "Error while deleting the item.";
+			System.err.println(e.getMessage());
+		}
+		return output;
+	}
 }
+
+
+
