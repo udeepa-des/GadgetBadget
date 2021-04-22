ef4be7ef3d866082bd968233459a8c572b69f0e7
PasinduManeka <manekaherat815@gmail.com>
2021-04-22 15:15:16 +0530

    create the insert method

--- GadgetBadget/src/com/FundService.java
+++ GadgetBadget/src/com/FundService.java
@@ -1,6 +1,9 @@
 package com;
 
+import javax.ws.rs.Consumes;
+import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
@@ -17,6 +20,19 @@ public class FundService {
 	@Produces(MediaType.TEXT_HTML)
 	public String readItem(){
 		return fund.readItems();
+	}
+	
+	@POST
+	@Path("/add")
+	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	@Produces(MediaType.TEXT_PLAIN)
+	public String insertData(@FormParam("companyName") String companyName,
+			@FormParam("researchID") int researchID,
+			@FormParam("researcherName") String researcherName,
+			@FormParam("investAmount") float investAmount,
+			@FormParam("description") String description) {
+		String output = fund.insertFunds(companyName, researchID, researcherName, investAmount, description);
+		return output;
 	}
 	
 }
