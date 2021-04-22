db77846c7e7384e0baeb15f607cb1ae1ac4aa3c0
HP <HP@DESKTOP-5OVQLNA>
2021-04-22 16:16:21 -0700

    Create the database connection

--- GadgetBadget/src/model/User.java
+++ GadgetBadget/src/model/User.java
@@ -1,5 +1,28 @@
 package model;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class User {
+	//A common method to connect to the Databse
+	private Connection Connect() {
+		Connection con = null;
+		
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			
+			//provide the correct details: DBServer/DBName, UserName, Password
+			con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/paf", "root", "");
+			
+		}
+		catch(Exception e)
+		{e.printStackTrace();}
+		
+		return con;
+	}
+	
+	
+	
+	
 
 }
