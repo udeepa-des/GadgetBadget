ad400a646ccee289be948075e848ff47e4b79f3a
pansal96 <sacheewa96@gmail.com>
2021-04-23 10:25:28 +0530

    created DB connection

--- GadgetBadget/src/model/Feedback.java
+++ GadgetBadget/src/model/Feedback.java
@@ -1,8 +1,30 @@
 package model;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class Feedback {
-	public String read() {
-		String output = "";
-		return output;
-	}
+	
+	// set connection
+	public Connection connect() {
+		Connection con = null;
+
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/exfeedback", "root", "");
+
+			// For testing
+			
+			System.out.print("Successfully connected");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return con;
+     }
+	
+	
+	
+	
+	
 }
