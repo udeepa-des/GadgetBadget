9f0232a8ba4524fa7e18692955e289fa98a3e7bd
pansal96 <sacheewa96@gmail.com>
2021-04-23 10:44:13 +0530

    created object to view feedback details

--- GadgetBadget/src/com/FeedbackService.java
+++ GadgetBadget/src/com/FeedbackService.java
@@ -1,5 +1,28 @@
 package com;
 
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import model.Feedback;
+
 public class FeedbackService {
 
+	// view feedback details
+	
+	Feedback fObj = new Feedback();
+	@GET
+	@Path("/")
+	@Produces(MediaType.TEXT_HTML)
+	public String readFb()
+	 {
+	 return fObj.readIFb();
+	 }
+	
+	
+	
+	
+	
+	
 }
