e80022251076e93dc808ed5fd394b9561b60a530
pansal96 <sacheewa96@gmail.com>
2021-04-23 10:46:53 +0530

    created object to insert feedback details

--- GadgetBadget/src/com/FeedbackService.java
+++ GadgetBadget/src/com/FeedbackService.java
@@ -1,6 +1,9 @@
 package com;
 
+import javax.ws.rs.Consumes;
+import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
@@ -20,7 +23,21 @@ public class FeedbackService {
 	 return fObj.readIFb();
 	 }
 	
-	
+	// insert feedback details
+
+	@POST
+	@Path("/")
+	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	@Produces(MediaType.TEXT_PLAIN)
+	public String insertFb(@FormParam("Name") String name,
+	 @FormParam("ContactNumber") String contactnum,
+	 @FormParam("Email") String email,
+	 @FormParam("Comment") String comment,
+	 @FormParam("overallExperience") String OvrlExp)
+	{
+	 String output = fObj.insertFb(name, contactnum, email, comment, OvrlExp);
+	return output;
+	}
 	
 	
 	
