70725bc3edda376d65b193fb2f5e1bd2ac66f034
pansal96 <sacheewa96@gmail.com>
2021-04-23 10:29:16 +0530

    created method to Insert Feedback Details(InsertFb)

--- GadgetBadget/src/model/Feedback.java
+++ GadgetBadget/src/model/Feedback.java
@@ -2,6 +2,7 @@ package model;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 
 public class Feedback {
 	
@@ -23,7 +24,41 @@ public class Feedback {
 		return con;
      }
 	
+	      // create method to Insert Feedback Details
 	
+	public String insertFb(String name, String contactnum, String email, String comment, String OvrlExp) {
+		String output = "";
+		try {
+			Connection con = connect();
+			if (con == null) {
+				return "Error while connecting to the database";
+			}
+			// create a prepared statement
+			
+			String query = " insert into feedb  (ID,Name,ContactNumber,Email,Comment,overallExperience)"
+					+ " values (?, ?, ?, ?, ?,?)";
+			PreparedStatement preparedStmt = con.prepareStatement(query);
+			
+			// binding values
+			
+			preparedStmt.setInt(1, 0);
+			preparedStmt.setString(2, name);
+			preparedStmt.setString(3, contactnum);
+			preparedStmt.setString(4, email);
+			preparedStmt.setString(5, comment);
+			preparedStmt.setString(6, OvrlExp);
+			
+			// execute the statement
+			
+			preparedStmt.execute();
+			con.close();
+			output = "Inserted successfully";
+		} catch (Exception e) {
+			output = "Error while inserting";
+			System.err.println(e.getMessage());
+		}
+		return output;
+	}
 	
 	
 	
