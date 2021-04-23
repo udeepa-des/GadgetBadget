67f744bbff949b98328aa5ddb77b9f801651bf2a
pansal96 <sacheewa96@gmail.com>
2021-04-23 10:49:11 +0530

    created object to update feedback details

--- GadgetBadget/src/com/FeedbackService.java
+++ GadgetBadget/src/com/FeedbackService.java
@@ -4,9 +4,13 @@ import javax.ws.rs.Consumes;
 import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 
 import model.Feedback;
 
@@ -39,6 +43,30 @@ public class FeedbackService {
 	return output;
 	}
 	
+	
+	//update feedback details
+	
+
+	@PUT
+	@Path("/")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.TEXT_PLAIN)
+	public String updateFb(String FeedbackData)
+	{
+	//Convert the input string to a JSON object
+		
+	 JsonObject feedbObject = new JsonParser().parse(FeedbackData).getAsJsonObject();
+	//Read the values from the JSON object
+	 
+	 String ID = feedbObject.get("ID").getAsString();
+	 String name = feedbObject.get("Name").getAsString();
+	 String contactnum = feedbObject.get("ContactNumber").getAsString();
+	 String email = feedbObject.get("Email").getAsString();
+	 String comment = feedbObject.get("Comment").getAsString();
+	 String OvrlExp = feedbObject.get("overallExperience").getAsString();
+	 String output = fObj.updateFb(ID, name, contactnum, email, comment, OvrlExp);
+	return output;
+	}
 	
 	
 	
