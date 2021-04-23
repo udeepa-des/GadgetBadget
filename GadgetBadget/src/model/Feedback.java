8823542cd090346fbd50eeb24f3c1cca86f3f257
pansal96 <sacheewa96@gmail.com>
2021-04-23 10:36:12 +0530

    created method to update feedback details(updateFb)

--- GadgetBadget/src/model/Feedback.java
+++ GadgetBadget/src/model/Feedback.java
@@ -121,6 +121,46 @@ public class Feedback {
 
 	}
 	
+	      //create method to update feedback details
+	
+		public String updateFb(String ID ,String name, String contactnum, String email, String comment, String OvrlExp)
+		{
+			 String output = "";
+			 try
+			 {
+			 Connection con = connect();
+			 if (con == null)
+			 {return "Error while connecting to the database for updating."; }
+			 
+			 // create a prepared statement
+			 
+			 String query = "UPDATE feedb SET Name=?,ContactNumber=?,Email=?,Comment=?,overallExperience=? WHERE ID=?";
+			 PreparedStatement preparedStmt = con.prepareStatement(query);
+			 
+			 // binding values
+			 
+			 preparedStmt.setString(1, name);
+			 preparedStmt.setString(2, contactnum);
+			 preparedStmt.setString(3, email);
+			 preparedStmt.setString(4, comment);
+			 preparedStmt.setString(5, OvrlExp);
+			 preparedStmt.setInt(6, Integer.parseInt(ID));
+			 
+			 // execute the statement
+			 
+			 preparedStmt.execute();
+			 con.close();
+			 output = "Updated successfully";
+			 }
+			 catch (Exception e)
+			 {
+			 output = "Error while updating the Feedback Details.";
+			 System.err.println(e.getMessage());
+			 }
+			 return output;
+			 } 
+		
+	
 	
 	
 	
