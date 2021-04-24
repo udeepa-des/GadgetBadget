f3239856cd9c003b12ff311dacfe979f9b27f7a9
udeepa-des <ududeepa@gmail.com>
2021-04-24 20:41:28 +0530

    cart changed

--- GadgetBadget/src/model/Cart.java
+++ GadgetBadget/src/model/Cart.java
@@ -3,14 +3,14 @@ package model;
 import java.sql.*;
 
 public class Cart {
-	//A common method to connect to the DB
+	//connecting to the DB
 	private Connection connect()
 	{
 		Connection con = null;
 		try
 		{
 			Class.forName("com.mysql.jdbc.Driver");
-			//Provide the correct details: DBServer/DBName, username, password
+			
 			con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/paf_project", "root", "");
 		}
 		catch (Exception e)
@@ -25,7 +25,7 @@ public class Cart {
 			Connection con = connect();
 			if (con == null)
 			{return "Error while connecting to the database for inserting."; }
-			// create a prepared statement
+			// insert query
 			String query = " insert into cart"
 					+ "			(`ID`,`researchID`,`researchName`,`Amount`,`Description`)"
 					+ " values (?, ?, ?, ?, ?)";
@@ -36,7 +36,7 @@ public class Cart {
 			preparedStmt.setString(3, name);
 			preparedStmt.setDouble(4, Double.parseDouble(price));
 			preparedStmt.setString(5, desc);
-			// execute the statement
+			// executing the statement
 			preparedStmt.execute();
 			con.close();
 			output = "Inserted successfully";
@@ -56,15 +56,14 @@ public class Cart {
 			Connection con = connect();
 			if (con == null)
 			{return "Error while connecting to the database for reading."; }
-			// Prepare the html table to be displayed
+			// Create the html table
 			output = "<table border='1'><tr><th>research ID</th><th>research Name</th>" +
 					"<th>Amount</th>" +
-					"<th>Description</th>" +
-					"<th>Update</th><th>Remove</th></tr>";
+					"<th>Description</th></tr>";
 			String query = "select * from cart";
 			Statement stmt = con.createStatement();
 			ResultSet rs = stmt.executeQuery(query);
-			// iterate through the rows in the result set
+			// iterating through the rows
 			while (rs.next())
 			{
 				String ID = Integer.toString(rs.getInt("ID"));
@@ -72,22 +71,15 @@ public class Cart {
 				String researchName = rs.getString("researchName");
 				String Amount = Double.toString(rs.getDouble("Amount"));
 				String Description = rs.getString("Description");
-				// Add into the html table
+				// Add values to the html table
 				output += "<tr><td>" + researchID + "</td>";
 				output += "<td>" + researchName + "</td>";
 				output += "<td>" + Amount + "</td>";
 				output += "<td>" + Description + "</td>";
-				// buttons
-				output += "<td><input name='btnUpdate' type='button' value='Update' "
-						+ "class='btn btn-secondary'></td>"
-								+ "<td><form method='post' action='items.jsp'>"
-								+ "<input name='btnRemove' type='submit' value='Remove'"
-								+ "class='btn btn-danger'>"
-										+ "<input name='ID' type='hidden' value='" + ID
-										+ "'>" + "</form></td></tr>";
+				
 			}
 			con.close();
-			// Complete the html table
+			// Complete the table
 			output += "</table>";
 		}
 		catch (Exception e)
@@ -106,7 +98,7 @@ public class Cart {
 			Connection con = connect();
 			if (con == null)
 			{return "Error while connecting to the database for updating."; }
-			// create a prepared statement
+			// update query
 			String query = "UPDATE cart SET researchID=?,researchName=?,Amount=?,Description=?"
 					+ "WHERE ID=?";
 							PreparedStatement preparedStmt = con.prepareStatement(query);
@@ -137,7 +129,7 @@ public class Cart {
 			Connection con = connect();
 			if (con == null)
 			{return "Error while connecting to the database for deleting."; }
-			// create a prepared statement
+			// delete query
 			String query = "delete from cart where ID=?";
 			PreparedStatement preparedStmt = con.prepareStatement(query);
 			// binding values
